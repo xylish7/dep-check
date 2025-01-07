e009815f86e2f74332eea98a6736d2a8e4166de3
Filip Frincu <filipfrincu@gmail.com>
2025-01-07 19:39:45 +0200

    feat: show dep and devdep updates

--- components/repo-card.tsx
+++ components/repo-card.tsx
@@ -1,11 +1,13 @@
+import { useState } from "react";
 import { Button } from "@nextui-org/button";
 import { Card, CardBody, CardFooter, CardHeader } from "@nextui-org/card";
-import { ArrowsClockwise } from "@phosphor-icons/react/dist/ssr";
-import { useState } from "react";
+import { button as buttonStyles } from "@nextui-org/theme";
+import { ArrowsClockwise, Eye } from "@phosphor-icons/react/dist/ssr";
 
-import { GithubReposRow, Updates } from "@/apis/supabase";
+import { Dependencies, GithubReposRow, Updates } from "@/apis/supabase";
 import { serverApi } from "@/apis/server";
 import { useNotification } from "@/providers/notification";
+import { Link } from "@nextui-org/link";
 
 interface RepoCardProps {
   repo: GithubReposRow;
@@ -14,11 +16,16 @@ interface RepoCardProps {
 export function RepoCard({ repo }: RepoCardProps) {
   const [updates, setUpdates] = useState<Updates | null>(repo.updates);
   const { showNotification } = useNotification();
-  const parsedJson = JSON.parse(repo.package_json);
-  const dependenciesCount = Object.keys(parsedJson.dependencies).length;
-  const devDependenciesCount = Object.keys(parsedJson.devDependencies).length;
+  const [isLoading, setIsLoading] = useState(false);
+
+  const parsedPackageJson = JSON.parse(repo.package_json);
+  const depCount = Object.keys(parsedPackageJson.dependencies).length;
+  const devDepCount = Object.keys(parsedPackageJson.devDependencies).length;
+  const depUpdatesCount = getCountPerVersionType(updates?.dependencies);
+  const devDepUpdatesCount = getCountPerVersionType(updates?.devDependencies);
 
   async function handleCheck() {
+    setIsLoading(true);
     const { data, error } = await serverApi.dependencies.get(repo.id);
 
     if (error) {
@@ -26,31 +33,97 @@ export function RepoCard({ repo }: RepoCardProps) {
         message: "Failed to check dependencies",
         color: "danger",
       });
+      setIsLoading(false);
       return;
     }
 
+    setIsLoading(false);
     setUpdates(data);
   }
 
-  console.log(updates);
   return (
     <Card className="p-2">
-      <CardHeader>{repo.name}</CardHeader>
+      <CardHeader className="text-lg font-semibold">{repo.name}</CardHeader>
       <CardBody>
-        <div>
-          <p>
-            <strong>Dependencies:</strong> {dependenciesCount}
-          </p>
-          <p>
-            <strong>Dev Dependencies:</strong> {devDependenciesCount}
-          </p>
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col text-sm gap-1">
+            <span className="text-default-500 font-semibold">Packages</span>
+            <div className="flex gap-4 text-sm">
+              <span>{depCount} deps</span>
+              <span>{devDepCount} dev deps</span>
+            </div>
+          </div>
+          <DependencyInfo title="Deps" count={depUpdatesCount} />
+          <DependencyInfo title="Dev Deps" count={devDepUpdatesCount} />
         </div>
       </CardBody>
-      <CardFooter className="justify-end">
-        <Button color="primary" size="sm" variant="flat" onPress={handleCheck}>
-          <ArrowsClockwise size={18} /> Check
+      <CardFooter className="gap-2 justify-end pt-6">
+        <Button
+          isLoading={isLoading}
+          size="sm"
+          variant="flat"
+          onPress={handleCheck}
+        >
+          {isLoading ? null : <ArrowsClockwise size={18} />} Check Updates
         </Button>
+        <Link
+          href="/account"
+          className={buttonStyles({ size: "sm", variant: "flat" })}
+        >
+          <Eye size={18} /> View
+        </Link>
       </CardFooter>
     </Card>
   );
+}
+
+interface DependencyInfoProps {
+  title: string;
+  count: {
+    major: number;
+    minor: number;
+    patch: number;
+    majorZero: number;
+  };
+}
+
+function DependencyInfo({ title, count }: DependencyInfoProps) {
+  return (
+    <div className="flex flex-col text-sm gap-1">
+      <span className="text-default-500 font-semibold">{title}</span>
+      <div className="flex gap-4 text-default-500">
+        <span className="text-rose-600">{count.major} major</span>
+        <span className="text-yellow-600">{count.minor} minor</span>
+        <span className="text-green-600">{count.patch} patch</span>
+        <span className="text-indigo-600">{count.majorZero} major-zero</span>
+      </div>
+    </div>
+  );
+}
+
+function getCountPerVersionType(dependencies?: Dependencies) {
+  const count = {
+    major: 0,
+    minor: 0,
+    patch: 0,
+    majorZero: 0,
+  };
+
+  if (!dependencies) {
+    return count;
+  }
+
+  Object.values(dependencies).forEach((dependency) => {
+    if (dependency.type === "major") {
+      count.major++;
+    } else if (dependency.type === "minor") {
+      count.minor++;
+    } else if (dependency.type === "patch") {
+      count.patch++;
+    } else if (dependency.type === "major-zero") {
+      count.majorZero++;
+    }
+  });
+
+  return count;
 }
