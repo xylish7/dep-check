9c450b219a5fbead60486dd068085e0b07b10ef7
Filip Frincu <filipfrincu@gmail.com>
2025-01-17 22:09:57 +0200

    feat: update host

--- config/env.ts
+++ config/env.ts
@@ -1,5 +1,8 @@
+import { siteConfig } from "./site";
+
 export const env = process.env.NODE_ENV as "development" | "production";
-export const host = env === "development" ? "http://localhost:3000" : "";
+export const host =
+  env === "development" ? "http://localhost:3000" : siteConfig.host;
 
 export const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
 export const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "";
