cd3147c113b014bae5c68babedc5abc49898a651
Filip Frincu <filipfrincu@gmail.com>
2025-09-09 19:00:07 +0300

    chore: remove heroui from tailwind.config

--- app/hero.ts
+++ app/hero.ts
@@ -1,3 +1,42 @@
 import { heroui } from "@heroui/theme";
 
-export default heroui();
+export default heroui({
+  themes: {
+    light: {
+      colors: {
+        primary: {
+          50: "#e1f6ef",
+          100: "#b7ead9",
+          200: "#8ddec3",
+          300: "#64d2ad",
+          400: "#3ac597",
+          500: "#10b981",
+          600: "#0d996a",
+          700: "#0a7854",
+          800: "#08583d",
+          900: "#053827",
+          foreground: "#000",
+          DEFAULT: "#10b981",
+        },
+      },
+    },
+    dark: {
+      colors: {
+        primary: {
+          50: "#053827",
+          100: "#08583d",
+          200: "#0a7854",
+          300: "#0d996a",
+          400: "#10b981",
+          500: "#3ac597",
+          600: "#64d2ad",
+          700: "#8ddec3",
+          800: "#b7ead9",
+          900: "#e1f6ef",
+          foreground: "#000",
+          DEFAULT: "#10b981",
+        },
+      },
+    },
+  },
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
-import { heroui } from "@heroui/theme";
-
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./providers/**/*.{js,ts,jsx,tsx,mdx}",
     "./utils/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,48 +11,6 @@ const config = {
     extend: {},
   },
   darkMode: "class",
-  plugins: [
-    heroui({
-      themes: {
-        light: {
-          colors: {
-            primary: {
-              50: "#e1f6ef",
-              100: "#b7ead9",
-              200: "#8ddec3",
-              300: "#64d2ad",
-              400: "#3ac597",
-              500: "#10b981",
-              600: "#0d996a",
-              700: "#0a7854",
-              800: "#08583d",
-              900: "#053827",
-              foreground: "#000",
-              DEFAULT: "#10b981",
-            },
-          },
-        },
-        dark: {
-          colors: {
-            primary: {
-              50: "#053827",
-              100: "#08583d",
-              200: "#0a7854",
-              300: "#0d996a",
-              400: "#10b981",
-              500: "#3ac597",
-              600: "#64d2ad",
-              700: "#8ddec3",
-              800: "#b7ead9",
-              900: "#e1f6ef",
-              foreground: "#000",
-              DEFAULT: "#10b981",
-            },
-          },
-        },
-      },
-    }),
-  ],
 };
 
 export default config;
